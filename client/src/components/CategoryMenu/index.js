ed75943b9a81946b9fe25f3da8cfd720eeb40112
NeoNeonilla <nelliafleurova@gmail.com>
2021-11-28 13:20:08 -0600

    tile button fade thing done

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -4,6 +4,9 @@ import { useQuery } from '@apollo/client';
 import { QUERY_CATEGORIES } from '../../utils/queries';
 import { useStoreContext } from "../../utils/GlobalState";
 import { idbPromise } from "../../utils/helpers";
+import { AnimationWrapper } from 'react-hover-animation';
+ 
+
 
 function CategoryMenu() {
   const [state, dispatch] = useStoreContext();
@@ -45,7 +48,9 @@ function CategoryMenu() {
 
       <br/>
       {categories.map((item) => (
+        <AnimationWrapper>
         <button className="btn success"
+
 
           key={item._id}
           onClick={() => {
@@ -54,6 +59,7 @@ function CategoryMenu() {
         >
           {item.name}
         </button>
+        </AnimationWrapper>
       ))}
     </div>
   );
